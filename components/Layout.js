4b773b1d2ffa70c17e87da692f732d2d96506fb3
randelramirez <randelramirez1@gmail.com>
2021-07-01 20:35:04 +0800

    Added Showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import styles from "@/styles/Layout.module.css";
 import Header from "./Header";
+import Showcase from "./Showcase";
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 export default function Layout({
   title = "DJ Events | Find the hottest parties",
@@ -9,6 +11,7 @@ export default function Layout({
   description = "Find the latest DJ and other musicla events",
   children,
 }) {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -17,6 +20,7 @@ export default function Layout({
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </>
