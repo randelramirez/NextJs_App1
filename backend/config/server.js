91cfe138f1ffbc212c6d0d0233841a4b574d82ac
randelramirez <randelramirez1@gmail.com>
2021-07-04 22:03:51 +0800

    refactor

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '8d11d7d22f38231d5444aac30a045d38'),
+      secret: env("ADMIN_JWT_SECRET", ""),
     },
   },
 });
